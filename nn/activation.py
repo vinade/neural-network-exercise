afe4ef0d51ec21614143a53fab55edb7d13944e4
Filipe Vinadé <vinade.filipe@gmail.com>
2022-02-02 18:37:20 -0300

    otimizações nas funções de ativação e adição da Tanh

--- nn/activation.py
+++ nn/activation.py
@@ -5,22 +5,22 @@ import numpy as np
 class ActivationFunction:
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         return x
 
     @staticmethod
-    def derivative(x):
+    def derivative(x, y=None):
         return np.ones(x.shape)
 
 
 class AFRelu(ActivationFunction):
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         return (x > 0) * x
 
     @staticmethod
-    def derivative(x):
+    def derivative(x, y=None):
         return (x > 0) * 1
 
 
@@ -29,29 +29,42 @@ class AFLeakyRelu(ActivationFunction):
     alpha = 0.001
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         return (x > 0) * x + (x <= 0) * AFLeakyRelu.alpha * x
 
     @staticmethod
-    def derivative(x):
+    def derivative(x, y=None):
         return (x > 0) * 1 + (x <= 0) * AFLeakyRelu.alpha
 
 
 class AFArcTan(ActivationFunction):
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         return 2.0 * math.atan(x)/math.pi
 
     @staticmethod
-    def derivative(x):
+    def derivative(x, y=None):
         return 2.0 / (math.pi * (1.0 + x*x))
+
+
+class AFTanh(ActivationFunction):
+
+    @staticmethod
+    def calc(x, y=None):
+        pz = np.exp(x)
+        nz = np.exp(-x)
+        return (pz - nz)/(pz + nz)
+
+    @staticmethod
+    def derivative(x, y=None):
+        return 1 - y * y
 
 
 class AFSigmoid(ActivationFunction):
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         px = x >= 0
         pz = np.logical_and(x < 0, np.exp(x))
         nz = np.logical_and(px, np.exp(-x))
@@ -61,27 +74,27 @@ class AFSigmoid(ActivationFunction):
         return dividend / divisor
 
     @staticmethod
-    def derivative(x):
-        return AFSigmoid.calc(x) * (1 - AFSigmoid.calc(x))
+    def derivative(x, y=None):
+        return y * (1 - y)
 
 
 class AFSoftmax(ActivationFunction):
 
     @staticmethod
-    def calc(x):
+    def calc(x, y=None):
         shift_x = x - np.max(x)
         exp = np.exp(shift_x)
-        return exp / np.sum(exp)
+        return exp / (np.sum(exp) / x.shape[0])
 
     @staticmethod
-    def derivative(x):
-        p = AFSoftmax.calc(x)
-        pT = AFSoftmax.calc(x.T)
-        return p @ np.identity(p.shape[0]) - pT @ p
+    def derivative(x, y=None):
+        X = AFSoftmax.calc(x)
+        return np.array([p @ np.identity(p.shape[0]) - p.T @ p for p in X])
 
 
 class ActivationFunctions:
 
+    Tanh = 'tanh'
     Relu = 'relu'
     ArcTan = 'arctan'
     Softmax = 'softmax'
@@ -89,6 +102,7 @@ class ActivationFunctions:
     LeakyRelu = 'leaky-relu'
 
     available = {
+        'tanh': AFTanh,
         'relu': AFRelu,
         'arctan': AFArcTan,
         'softmax': AFSoftmax,
